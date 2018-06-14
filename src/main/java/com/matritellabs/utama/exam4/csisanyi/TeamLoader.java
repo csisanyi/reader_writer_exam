4b207c32b73f6b228097e49a515a78fedec4b03a
sanyi <csisanyi@gmail.com>
2018-06-14 13:19:05 +0200

    final commit after javadoc creation

--- src/main/java/com/matritellabs/utama/exam4/csisanyi/TeamLoader.java
+++ src/main/java/com/matritellabs/utama/exam4/csisanyi/TeamLoader.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.ParseException;
@@ -20,66 +21,124 @@ public class TeamLoader {
 
     private static Logger log = LoggerFactory.getLogger(TeamLoader.class);
 
+    /** Reader methods @throws IOException or Parsexception, RuntimeException,
+     * The method accepts 2 parameters
+     * based on the parameters it decides,
+     * how to read in from the file
+     * based on the read data it creates
+     * new AgileTeam objects and
+     * adds them to a list*/
+
     public static List<AgileTeam> readTeamsFromFile(List<String> filePathList, TeamFileFormat fileFormat)
     throws IOException, ParseException, RuntimeException {
 
         List<AgileTeam> returnList = new ArrayList<>();
         SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd");
 
+        //* Method, if the TeamFilformat paramaeter is ONE_FILE*/
         if(fileFormat.equals(ONE_FILE)) {
+            try {
+                Path theFile = Paths.get(filePathList.get(0));
+                log.info("reading input file");
+                List<String> lineList = Files.readAllLines(theFile);
 
-            Path theFile = Paths.get(filePathList.get(0));
-            List<String> lineList = Files.readAllLines(theFile);
 
-            for(int i = 0; i < lineList.size(); i++) {
-                String[] splitted = lineList.get(i).split(";");
-                int sizeOfMembersList = Integer.parseInt(splitted[2]);
-                if(splitted.length > 3 + sizeOfMembersList) {
-                    throw new RuntimeException();
+                for (int i = 0; i < lineList.size(); i++) {
+                    String[] splitted = lineList.get(i).split(";");
+                    int sizeOfMembersList = Integer.parseInt(splitted[2]);
+                    if (splitted.length > 3 + sizeOfMembersList) {
+                        String message = " more ; sysmbols than expected";
+                        log.error(message);
+                        throw new RuntimeException(message);
+                    }
+                    List<String> membersList = new ArrayList<>();
+                    if (splitted.length < 3 + sizeOfMembersList) {
+                        String message = "Less members than expected";
+                        log.error(message);
+                        throw new RuntimeException(message);
+                    }
+                    for (int j = 3; j < splitted.length; j++) {
+                        membersList.add(splitted[j]);
+                    }
+                    log.info("Creating new AgileTeaminstance, and adding it to the list");
+                    returnList.add(new AgileTeam(splitted[0], SDF.parse(splitted[1]), sizeOfMembersList, membersList));
                 }
-                List<String> membersList = new ArrayList<>();
-                for (int j = 3; j < splitted.length; j++) {
-                    membersList.add(splitted[j]);
-                }
 
-                returnList.add(new AgileTeam(splitted[0],SDF.parse(splitted[1]), sizeOfMembersList,membersList));
+            } catch (IOException e) {
+                String message = "File not found";
+                log.error(message);
+                throw new FileNotFoundException(message);
             }
-
         }
 
+        //* Method, if the TeamFilformat paramaeter is TWO_FILES*/
         if(fileFormat.equals(TWO_FILES)) {
+            try {
+                Path teamData = Paths.get(filePathList.get(0));
+                log.info("reading input files");
+                List<String> lineList = Files.readAllLines(teamData);
+                Path nameAndlist = Paths.get(filePathList.get(1));
+                List<String> lineList2 = Files.readAllLines(nameAndlist);
 
-            Path teamData = Paths.get(filePathList.get(0));
-            List<String> lineList = Files.readAllLines(teamData);
-            Path nameAndlist = Paths.get(filePathList.get(1));
-            List<String> lineList2 = Files.readAllLines(nameAndlist);
+                if (lineList.size() != lineList2.size()) {
+                    String message = "The two files do not correspond to each other";
+                    log.error(message);
+                    throw new RuntimeException(message);
+                }
 
-            if (lineList.size() != lineList2.size()) {
-                throw new RuntimeException();
+
+                for (int i = 0; i < lineList.size(); i++) {
+                    String[] splitted = lineList.get(i).split(";");
+                    String[] splitted2 = lineList2.get(i).split(";");
+
+                    if (splitted.length > 3) {
+                        String message = " more ; sysmbols than expected";
+                        log.error(message);
+                        throw new RuntimeException(message);
+                    }
+
+                    int sizeOfMembersList = Integer.parseInt(splitted[2]);
+                    List<String> membersList = new ArrayList<>();
+
+                    if (splitted2.length > sizeOfMembersList + 1) {
+                        String message = " more ; sysmbols than expected";
+                        log.error(message);
+                        throw new RuntimeException(message);
+                    }
+
+
+
+
+
+                    if (!splitted[0].equals(splitted2[0])) {
+                        String message = "Teams are not the same";
+                        log.error(message);
+                        throw new RuntimeException(message);
+                    }
+
+                    for (int j = 1; j < splitted2.length; j++) {
+                        membersList.add(splitted2[j]);
+                    }
+
+                    returnList.add(new AgileTeam(splitted[0], SDF.parse(splitted[1]), sizeOfMembersList, membersList));
+                }
+
+            } catch (IOException io) {
+                String message = "File not found";
+                log.error(message);
+                throw new FileNotFoundException(message);
             }
-
-            for(int i = 0; i < lineList.size(); i++) {
-                String[] splitted = lineList.get(i).split(";");
-                String[] splitted2 = lineList2.get(i).split(";");
-
-                int sizeOfMembersList = Integer.parseInt(splitted[2]);
-                List<String> membersList = new ArrayList<>();
-
-                if(!splitted[0].equals(splitted2[0])) {
-                    throw new RuntimeException();
-                }
-
-                for (int j = 1; j < splitted2.length; j++) {
-                    membersList.add(splitted2[j]);
-                }
-
-                returnList.add(new AgileTeam(splitted[0],SDF.parse(splitted[1]), sizeOfMembersList,membersList));
-                }
         }
+        log.info("method executed correctly");
         return returnList;
 
     }
 
+    /**File writer method @throws IOException
+     * based on the input parameters
+     * it writes the data of the input Lists
+     * objects to a file, in a format
+     * described by the fileformat parameter*/
     public static void writeTeamToFile(List<String> filePathlist, TeamFileFormat fileFormat,
                                        String teamName, Date creationDate, int numberOfMembers, List<String> listOfMembers )
     throws IOException {
@@ -87,6 +146,7 @@ public class TeamLoader {
 
         SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd");
 
+        //* Method, if the TeamFilformat paramaeter is ONE_FILE*/
         if (fileFormat.equals(ONE_FILE)) {
             Path fileToWrite = Paths.get(filePathlist.get(0));
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileToWrite.toFile()));
@@ -99,8 +159,9 @@ public class TeamLoader {
 
             writer.write(teamName + ";" + formattedDate + ";" + numberOfMembers + ";" + membersString);
             writer.close();
+            log.info("method executed correctly");
         }
-
+        //* Method, if the TeamFilformat paramaeter is TWO_FILES*/
         if(fileFormat.equals(TWO_FILES)) {
             Path fileToWrite = Paths.get(filePathlist.get(0));
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileToWrite.toFile()));
@@ -118,6 +179,7 @@ public class TeamLoader {
 
             writer.close();
             writer2.close();
+            log.info("method executed correctly");
         }
     }
 }
--- src/main/java/com/matritellabs/utama/exam4/csisanyi/main.java
+++ src/main/java/com/matritellabs/utama/exam4/csisanyi/main.java
@@ -10,24 +10,50 @@ public class main {
 
     public static void main(String[] args) {
 
-        List<String> fileList = new ArrayList<>();
-        fileList.add("/home/sanyi/workspace/agile_team_handler/src/main/java/com/matritellabs/utama/exam4/resources/write_twofile_file1.txt");
-        fileList.add("/home/sanyi/workspace/agile_team_handler/src/main/java/com/matritellabs/utama/exam4/resources/write_twofile_file2.txt");
-        List<String> memberList = new ArrayList<>();
-        memberList.add("Sandor");
-        memberList.add("Jozsef");
-        memberList.add("Benedek");
+//        List<String> fileList = new ArrayList<>();
+////        fileList.add("/home/sanyi/workspace/agile_team_handler/src/main/java/com/matritellabs/utama/exam4/resources/write_twofile_file1.txt");
+////        fileList.add("/home/sanyi/workspace/agile_team_handler/src/main/java/com/matritellabs/utama/exam4/resources/write_twofile_file2.txt");
+//        fileList.add("/home/sanyi/workspace/agile_team_handler/src/main/java/com/matritellabs/utama/exam4/resources/agile_one.txt");
+//        List<String> memberList = new ArrayList<>();
+//        memberList.add("Sandor");
+//        memberList.add("Jozsef");
+//        memberList.add("Benedek");
+//
+//        try {
+//            TeamLoader.readTeamsFromFile(fileList,TeamFileFormat.ONE_FILE);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+////        try {
+//////            TeamLoader.readTeamsFromFile(fileList, TeamFileFormat.TWO_FILES);
+////            SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd");
+////            TeamLoader.writeTeamToFile(fileList,TeamFileFormat.TWO_FILES,"TeamHozzakAMeleget",SDF.parse("1986-08-27"),3,memberList);
+////        } catch (IOException e) {
+////            e.printStackTrace();
+////        } catch (ParseException e) {
+////            e.printStackTrace();
+////        }
 
-
-        try {
-//            TeamLoader.readTeamsFromFile(fileList, TeamFileFormat.TWO_FILES);
-            SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd");
-            TeamLoader.writeTeamToFile(fileList,TeamFileFormat.TWO_FILES,"TeamHozzakAMeleget",SDF.parse("1986-08-27"),3,memberList);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
+//        fileList.add("/home/sanyi/workspace/agile_team_handler/src/main/java/com/matritellabs/utama/exam4/resources/agile_onefile.txt");
+//        try {
+//            TeamLoader.readTeamsFromFile(fileList,TeamFileFormat.ONE_FILE);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        fileList.add("/home/sanyi/workspace/agile_team_handler/src/main/java/com/matritellabs/utama/exam4/resources/agile_twofile_file1.txt");
+//        fileList.add("/home/sanyi/workspace/agile_team_handler/src/main/java/com/matritellabs/utama/exam4/resources/agile_twofile_file2.txt");
+//        try {
+//            TeamLoader.readTeamsFromFile(fileList,TeamFileFormat.TWO_FILES);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
     }
 }
